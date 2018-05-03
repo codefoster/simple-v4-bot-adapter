4dc472667cbe816cc254a61524b2656dbe890880
Jeremy Foster <jeremy.foster@live.com>
2018-05-03 16:55:09 -0700

    update to use botframeworkadapter

--- index.ts
+++ index.ts
@@ -5,3 +5,21 @@ new ConsoleAdapter().listen(ctx => {
     if (ctx.activity.text === 'message') 
         ctx.sendActivity(ctx.activity.text);
 })
+
+import { BotFrameworkAdapter } from 'botbuilder';
+import * as restify from 'restify';
+
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+const adapter = new BotFrameworkAdapter();
+
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async ctx => {
+        if (ctx.activity.type === 'message') {
+            ctx.sendActivity(ctx.activity.text);
+        }
+    });
+});
